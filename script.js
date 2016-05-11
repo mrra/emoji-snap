f92037757d65f56dd8b468acfb189447361a433a
Rich Armstrong <mr.richard.armstrong@gmail.com>
2016-05-11 13:53:50 +0200

    Did the snap function

--- script.js
+++ script.js
@@ -32,7 +32,46 @@ var getRandomEmoji = function () {
 	var randomEmoji = emojis[randomNumber];
 	
 	return randomEmoji;
-}
+};
+
+var snap = function (pcCalledSnap) {
+	// compare bewteen the 2 emojis
+	// if it's a joker then snap is true!
+	// if snap is true then the user/pc get's +1 to roundsWon
+	// if user gets it wrong then pc gets +1 to roundsWon
+	// notify of what just happened – console/ui/html
+	
+	var snap = pc.currentEmoji == user.currentEmoji;
+	
+	if (pc.currentEmoji == joker || user.currentEmoji == joker) {
+		snap = true;
+	}
+	
+	console.group('Snap Called by: '+ (pcCalledSnap ? 'PC' : 'User'));
+	
+	// pc called
+	if (pcCalledSnap) {
+		if (snap) {
+			pc.roundsWon ++;
+			console.log('pc won the round')
+		} else {
+			user.roundsWon ++;
+			console.log('pc lost the round')
+		}
+	
+	// user called
+	} else {
+		if (snap) {
+			user.roundsWon ++;
+			console.log('user won the round')
+		} else {
+			pc.roundsWon ++;
+			console.log('user lost the round')
+		}
+	}
+	
+	console.groupEnd();
+};
 
 
 
