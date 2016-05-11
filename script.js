b5b886ed0b90d99f99be99c027eb7cbc7e448b56
Rich Armstrong <mr.richard.armstrong@gmail.com>
2016-05-11 13:29:29 +0200

    setup startNextRound function

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-console.log('Hey good looking!');
+// define players
+var user = {
+	name: 'user',
+	roundsWon: 0,
+	currentEmoji: undefined
+};
+var pc = {
+	name: 'pc',
+	roundsWon: 0,
+	currentEmoji: undefined
+};
+
+// define our emojis
+var emojis = ['😎','🐰','🔫','🃏','😜','💩'];
+var joker = '🃏';
+
+// define state
+var currentRound = 0;
+
+var startNextRound = function () {
+	currentRound ++;
+	
+	// set players emojis
+	pc.currentEmoji = getRandomEmoji();
+	user.currentEmoji = getRandomEmoji();
+	
+	console.log('emojis', pc.currentEmoji, 'vs', user.currentEmoji)
+};
+
+var getRandomEmoji = function () {
+	var randomNumber = Math.round(Math.random() * (emojis.length-1));
+	var randomEmoji = emojis[randomNumber];
+	
+	return randomEmoji;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
